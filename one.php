e4646bfbcf8a0575c63ce7728c21cb888ed42054
emiax <mail@emilaxelsson.se>
2014-01-08 21:42:16 +0100

    fixed date bug

--- one.php
+++ one.php
@@ -7,6 +7,7 @@ $id = isset($_GET['post']) ? $_GET['post'] : 0;
 
 if (isset($articles[$id])) {
   $article = $articles[$id];
+  $d = niceDate($article['date']);
   echo '<article><h3 class="blogpost-date">' . $d . '</h3><h2><a href="' . permalink($id) . '">' . $article['title'] . '</a></h2>' . articleContents($id) . '</article>';
 }
 
